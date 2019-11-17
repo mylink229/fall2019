1de22b75add980b1a69fe9dcc9c3c7a01fd722de
Tswjfwmeng Vang <tcvang.229@gmail.com>
2019-11-17 15:19:23 -0600

    added HW and jentry

---
+++ cs353/homework/hw5/hw5.asm
@@ -0,0 +1,22 @@
+    segment .data
+a db 00000011b
+b db 00000010b
+c db 00000000b 
+
+    segment .text
+    global main
+main:
+    mov ax, [a]
+    mov cx, [b]
+    mov dx, [c]
+
+    imul ax, [a]
+    imul cx, [b]
+
+    add dx, ax
+    add dx, cx
+
+    mov [c], dx
+    ret 
+
+
